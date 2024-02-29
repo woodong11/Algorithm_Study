71f6d3acb5d17e25b0a349464c342cafb0e997e4
knamk7 <97281281+knamk7@users.noreply.github.com>
2024-02-29 14:09:46 +0900

    가지치기 조건 추가, 21ms

--- SWEA/1767_프로세서연결하기/knamk7.cpp
+++ SWEA/1767_프로세서연결하기/knamk7.cpp
@@ -139,7 +139,7 @@ int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
