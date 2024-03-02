8df35fd5558dd4dc6c0fdf5a9ee2fe78c0627b80
biginnings <144890310+biginnings@users.noreply.github.com>
2024-03-02 17:54:00 +0900

    Update jongha(timeover).cpp

--- SWEA/3234_준환이의양팔저울/jongha(timeover).cpp
+++ SWEA/3234_준환이의양팔저울/jongha(timeover).cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// 시간 줄이기 위해 한 행동
+// vector 썼었는데 배열로 바꿈 -> 테케 돌렸을 때 0.1 초 정도 줄어듦.
+// N - 1 일 때 temp == 0 이면 templeftSum + 마지막 남은 숫자가 temprightSum 보다 작거나 같을 때
+// sum + 1 하고 temprightSum이 한줄인 경우의 수 를 더한 값을 출력하는 방법을 생각했는데
+// 오히려 시간 더 늘어남.
+
 int grid[9];
 int T, N, sum;
 int visited[9];
