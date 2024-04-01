43e1ec3f9c3c5ec09e4cf041cfbd74264733a8d0
rjsrnr <133875265+rjsrnr@users.noreply.github.com>
2024-04-01 15:43:36 +0900

    Update gg8099.cpp

--- baekjoon/15684 사다리 조작/gg8099.cpp
+++ baekjoon/15684 사다리 조작/gg8099.cpp
@@ -7,7 +7,6 @@ struct xy {
 	int y, x;
 };
 int N, M, H;
-int visited[31][11];
 vector<xy> vec;
 int num;
 
@@ -19,17 +18,13 @@ int func(int(&tmp)[31][11]) {
 			if (tmp[a][b] == 0) {
 				a++;
 			}
-			else {
-				int tmp_b_mi = b - 1;
-				int tmp_b_pl = b + 1;
-				if (tmp_b_mi >= 0 && tmp[a][tmp_b_mi] == 1){
-					a++;
-					b--;
-				}
-				else if (tmp_b_pl < N && tmp[a][tmp_b_pl] == 1){
-					a++;
-					b++;
-				}
+			else if (tmp[a][b] == 1) {
+				a++;
+				b++;
+			}
+			else if (tmp[a][b] == 2) {
+				a++;
+				b--;
 			}
 			if (a == H) {
 				if (i == b)
@@ -45,7 +40,7 @@ int func(int(&tmp)[31][11]) {
 	}
 	return flag;
 }
-void dfs(int(&tmp)[31][11], int level, int goal) {
+void dfs(int(&tmp)[31][11], int level, int goal, int(&visited)[31][11]) {
 	if (num == 1)
 		return;
 	if (level == goal) {
@@ -53,40 +48,49 @@ void dfs(int(&tmp)[31][11], int level, int goal) {
 		return;
 	}
 	int tp_map[31][11];
+	int tp_visited[31][11];
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < N; j++) {
 			tp_map[i][j] = tmp[i][j];
+			tp_visited[i][j] = visited[i][j];
 		}
 	}
 	for (int i = 0; i < N - 1; i++) {
 		for (int j = 0; j < H; j++) {
-			if (tp_map[i][j] == 1)
+			if (tp_map[j][i] != 0 || tp_map[j][i + 1] != 0 )
 				continue;
-			if (visited[i][j] == 1) {
+			
+			if (tp_visited[j][i] == 1) {
 				continue;
 			}
-			visited[i][j] = 1;
-			tp_map[i][j] = 1;
-			tp_map[i][j + 1] = 1;
-			dfs(tp_map, level + 1, goal);
-			tp_map[i][j] = 0;
-			tp_map[i][j + 1] = 0;
+			tp_visited[j][i] = 1;
+			tp_map[j][i] = 1;
+			tp_map[j][i + 1] = 2;
+			dfs(tp_map, level + 1, goal, tp_visited);
+			if (num == 1)
+				break;
+			tp_map[j][i] = 0;
+			tp_map[j][i + 1] = 0;
 		}
+		if (num == 1)
+			break;
 	}
+	if (num == 1)
+		return;
 
 }
 
 int main() {
 	cin >> N >> M >> H;
 	int map[31][11] = { 0, };
+	int visited[31][11] = { 0, };
 	for (int i = 0; i < M; i++) {
 		int from, to;
 		cin >> from >> to;
-		vec.push_back({ from,to });
-		vec.push_back({ from, to + 1 });
-	}
-	for (int i = 0; i < vec.size(); i++) {
-		map[vec[i].y][vec[i].x] = 1;
+		vec.push_back({ from - 1 ,to - 1 });
+		map[from - 1][to - 1] = 1;
+		vec.push_back({ from - 1, to });
+		map[from - 1][to] = 2;
 	}
 	int result = func(map);
 	if (result == 1) {
@@ -94,7 +98,7 @@ int main() {
 	}
 	else {
 		for (int i = 1; i <= 3; i++) {
-			dfs(map, 0, i);
+			dfs(map, 0, i, visited);
 			if (num == 1) {
 				cout << i;
 				result = num;
