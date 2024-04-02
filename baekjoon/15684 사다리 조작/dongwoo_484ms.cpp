5e598463ca5fba0f7013ea33136d5ebe37bdae36
DONG WOO LEE <eastandrain@gmail.com>
2024-04-02 22:58:35 +0900

    Update and rename dongwoo_failed.cpp to dongwoo_484ms.cpp

--- baekjoon/15684 사다리 조작/dongwoo_484ms.cpp
+++ baekjoon/15684 사다리 조작/dongwoo_484ms.cpp
@@ -12,23 +12,23 @@ void input() {
     for (int i = 0; i < M; i++)
     {
         cin >> a >> b;
-        MAP[a][b] = 1; 
-        MAP[a][b + 1] = 1;
+        MAP[a][b] = 1;
+        MAP[a][b + 1] = 2;     
     }
 }
 
-// 맵에서 사다리 타고 체크하는 함수
+// 맵에서 사다리 타고 체크하는 타고, 2를 만나면 왼쪽으로 타게 된다.
 bool check() {
-    for (int i = 1; i <= N; i++){
+    for (int i = 1; i <= N; i++) {
         int col = i;
         int row = 0;
         while (row <= H) {
-            if (MAP[row][col] == 1) {
-                if (col -1>= 1 && MAP[row][col - 1] == 1)    // 왼쪽으로 가는 경우
-                    col--;
-                else if (col +1 <= N && MAP[row][col + 1] == 1) // 오른쪽으로 가는 경우
-                    col++;
-            }    
+            if (MAP[row][col] == 1) {       // 오른쪽으로 가는 경우
+                col++;
+            }
+            else if (MAP[row][col] == 2) {  // 왼쪽으로 가는 경우
+                col--;
+            }
             row++;
         }
         if (col != i)
@@ -46,22 +46,23 @@ void func(int level, int preRow, int preCol) {
         return;
     }
     // for문 변수 설정 이렇게 해주면 j돌때 이전에 돌았던 거 다시 안돌아도 된다
+    // 첫 i의 j만 preCol부터 시작하고, 그 다음번엔 정해둔 1부터 시작한다.
     // -> 조합 구현이 가능해서 가지치기 효과를 낸다!!
-    for (int i = preRow; i <= H; i++, preCol = 1){
-        for (int j = preCol; j <= N; j++){
-            if (MAP[i][j] == 1)
+    for (int i = preRow; i <= H; i++, preCol = 1) {
+        for (int j = preCol; j <= N-1; j++) {
+            if (MAP[i][j])
                 continue;
-            if ((j - 1 >= 1) && (MAP[i][j - 1] == 1))    // 왼쪽 체크
+            if ((MAP[i][j - 1])== 1)    // 왼쪽 체크
                 continue;
-            if ((j + 2 <= N) && (MAP[i][j + 2] == 1))    // 오른쪽 체크
+            if (MAP[i][j + 1] == 1)    // 오른쪽 체크
                 continue;
 
             MAP[i][j] = 1;
-            MAP[i][j + 1] = 1;
+            MAP[i][j + 1] = 2;
             func(level + 1, i, j);
             MAP[i][j] = 0;
             MAP[i][j + 1] = 0;
-        
+
         }
     }
 }
@@ -69,7 +70,7 @@ void func(int level, int preRow, int preCol) {
 void solve() {
 
     // 0번, 1번, 2번, 3번 재귀타는 코드
-    for (int i = 0; i <= 3; i++){
+    for (int i = 0; i <= 3; i++) {
         target = i;
         func(0, 1, 1);
         if (answer != -1) {
@@ -82,9 +83,6 @@ void solve() {
 
 int main() {
     freopen("sample_input.txt", "r", stdin);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
 
     input();
     solve();
