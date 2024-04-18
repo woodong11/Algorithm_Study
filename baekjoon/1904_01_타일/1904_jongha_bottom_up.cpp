fb365da74645f7c0c9bea2e87a7550c8d8bfc664
biginnings <144890310+biginnings@users.noreply.github.com>
2024-04-18 16:33:14 +0900

    8ms

---
+++ baekjoon/1904_01_타일/1904_jongha_bottom_up.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+long long memo[1000001];
+
+int T, a;
+int main() {
+	cin >> T;
+
+	memo[1] = 1;
+	memo[2] = 2;
+
+	for (int i = 3; i < 1000001; i++) {
+		memo[i] = (memo[i - 2] + memo[i - 1]) % 15746;
+	}
+
+	cout << memo[T];
+
+	return 0;
+}
