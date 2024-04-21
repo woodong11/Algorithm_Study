efec25e68df25391a10aa466426fbbfda1743ce8
knamk7 <97281281+knamk7@users.noreply.github.com>
2024-04-21 18:00:29 +0900

    Fastest solution

---
+++ baekjoon/1904_01타일/1904_knamk7_optimal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int arr[1000001] = { 1, 1, 2 };
+
+int main()
+{
+	int n;
+	cin >> n;
+    n %= 47244;
+	for (int i = 3; i <= n; i++)
+	{
+		arr[i] = (arr[i - 2] + arr[i - 1]) % 15746;
+	}
+	cout << arr[n];
+	return 0;
+}
